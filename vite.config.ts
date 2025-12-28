df6a8dd8ac503069aa0e92aeea8f141e8c76a44b
Balaji mac <b@gmail.com>
2025-12-28 14:30:37 +0530

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-
+// vite.config.ts
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true'
-    ? '/idhrona-landing-site/'
-    : '/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
-  }
-})
+    assetsDir: 'assets',
+  },
+});
